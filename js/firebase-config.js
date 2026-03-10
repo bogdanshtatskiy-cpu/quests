647d90933daf4c2b9025c6160393f107b7668a23
bogdanshtatskiy-cpu <bogdan.shtatskiy@gmail.com>
2026-03-10 21:26:09 +0200

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+// js/firebase-config.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyAFB-_G1GwAIdd-BH6oFT83Es9wUpUGbWU",
+    authDomain: "minecraftquests.firebaseapp.com",
+    projectId: "minecraftquests",
+    storageBucket: "minecraftquests.firebasestorage.app",
+    messagingSenderId: "829565515399",
+    appId: "1:829565515399:web:1bcdee2f4fb56601162a22"
+};
